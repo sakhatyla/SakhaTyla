9e6b4a5408f6342478e9bf26005d8cb9eaeab024
Vassily Ivanov <vivanov@sharplogic.ru>
2022-09-05 16:00:30 +0900

    Add translate box letter button handlers

---
+++ SakhaTyla.Web.Front/wwwroot/js/translate.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $('.translate-letters button').click(function () {
+        var newText = $(this).text();
+        var query = $('#query');
+        var queryElement = query[0];
+
+        var start = queryElement.selectionStart;
+        var end = queryElement.selectionEnd;
+
+        var before = queryElement.value.slice(0, start);
+        var after = queryElement.value.slice(end);
+
+        var text = before + newText + after;
+        queryElement.value = text;
+
+        $('#query').keydown();
+    });
+});
